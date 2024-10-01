ede0e7394c568cafd7b91ef58da8a1b9792a681f
Richard Garber <rg.1029384756@gmail.com>
2024-09-30 21:55:06 -0400

    Save function that I wrote for OpenSUSE

---
+++ opensuse.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+# An alias and function that I use with OpenSUSE.
+function cleanup {
+  local unneeded_packages=`zypper --non-interactive --quiet pa --unneeded | awk -F '|' 'NR>2 { print $3 }' - | tr '\n' ' '`
+if [[ $unneeded_packages ]]; then
+  sudo zypper rm $unneeded_packages
+fi
+}
+alias update='sudo zypper dup && sudo zypper patch && sudo zypper up'
