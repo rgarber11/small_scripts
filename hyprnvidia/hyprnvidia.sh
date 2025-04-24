297d225aa7a9198fb7ece412bac7b4d26916c439
Richard Garber <rg.1029384756@gmail.com>
2025-04-24 09:33:18 -0400

    Hopefully better hardware acceleration support on dGPU

--- hyprnvidia/hyprnvidia.sh
+++ hyprnvidia/hyprnvidia.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if test -f /etc/X11/xorg.conf.d/10-nvidia.conf; then
-    printf '%s\n' 'env = LIBVA_DRIVER_NAME,nvidia' 'env = XDG_SESSION_TYPE,wayland' 'env = GBM_BACKEND,nvidia-drm' 'env = __GLX_VENDOR_LIBRARY_NAME,nvidia' 'cursor {' 'use_cpu_buffer = true' '}' >/home/rgarber11/.config/hypr/nvidia.conf
+    printf '%s\n' 'env = LIBVA_DRIVER_NAME,nvidia' 'env = XDG_SESSION_TYPE,wayland' 'env = GBM_BACKEND,nvidia-drm' 'env = __GLX_VENDOR_LIBRARY_NAME,nvidia' 'env = NVD_BACKEND,direct' 'cursor {' 'use_cpu_buffer = true' '}' >/home/rgarber11/.config/hypr/nvidia.conf
 else
     echo "" >/home/rgarber11/.config/hypr/nvidia.conf
 fi
